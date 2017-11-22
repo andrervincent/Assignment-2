9260efe7906b3489718e2c0ab509daa908a3b277
André Vincent <andre.vincent@bison.howard.edu>
2017-11-21 22:54:37 -0500

    Adds algorithm needed without the subprogram specification

---
+++ assignment2.s
@@ -0,0 +1,8 @@
+#Find way to take in up to 1000 characters as input
+#String can consist of one or more susbstrings with comma in between
+#For each substring - if it is a hexadecimal string, the program prints out
+#the corresponding unsigned decimal integer.
+#If the hexadecimal string has more than 8 characters,
+#print out "too large".
+#Otherwise the program prints out the string of "NaN". Separate output by commas.
+
