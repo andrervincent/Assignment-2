6cc0f38588e813dbb0eceaf9d3b1e9f9e0a9acdf
André Vincent <andre.vincent@bison.howard.edu>
2017-11-22 22:02:15 -0500

    Adds algorithm for subprograms.

--- assignment2.s
+++ assignment2.s
@@ -6,3 +6,23 @@
 #print out "too large".
 #Otherwise the program prints out the string of "NaN". Separate output by commas.
 
+#main:
+#call string_to_int and display_int
+
+
+#hex_to_dec:
+#This subprogram converts one hex character to decimal.
+#Uses registers to pass parameters into.
+#Values are returned with registers.
+
+#string_to_int:
+#This subprogram converts the hex string to a decimal.
+#Has to call hex_to_dec to get the decimal value of each
+#of the characters.
+#Return values with the stack.
+
+#Display_int:
+#Displays an unsigned integer that is calculated from other subprograms
+#Stack must be used to pass parameters into the subprogram.
+#Don't return any values.
+
